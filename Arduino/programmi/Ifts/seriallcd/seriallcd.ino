32c2c2d8b97686055d5b97953e90e67eeb363e91
Alberto Panu <alberto@panu.it>
2021-02-09 13:03:03 +0100

    Update seriallcd.ino

--- Arduino/programmi/Ifts/seriallcd/seriallcd.ino
+++ Arduino/programmi/Ifts/seriallcd/seriallcd.ino
@@ -1,12 +1,11 @@
 // include the library code:
 #include <LiquidCrystal.h>
 
-
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup() {
-  lcd.begin(16, 2);
   // put your setup code here, to run once:
+  lcd.begin(16, 2);
   Serial.begin(9600);
   Serial.println("inserisci una stringa di massimo 16 caratteri");
 }
@@ -18,12 +17,14 @@ void loop() {
     Serial.println(stringa);
     lcd.setCursor(0, 0);
     lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
     //lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(stringa);
   }
 
-  lcd.setCursor(0, 1);
+  //lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  //lcd.print(millis() / 1000);
 }
